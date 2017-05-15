ad9c23332f27ea59d8aaf11b7965bba912d9f734
GillesULCO <gmachure@gmail.com>
2017-05-15 11:21:44 +0200

    logs close #2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
-int main(){
+#include <glog/logging.h>
+int main(int argc, char ** argv){
+	google::InitGoogleLogging(argv[0]);
+    google::SetLogDestination(google::GLOG_INFO, "log_chifoumi");
 	std::cout << "Hello World !" << std::endl;
+	LOG(INFO) << "Test log Hello World";
 	return 0;
 }
